27ae360c398cc716f315a8116261ab385a7c96d7
Gabriel Saad <g.augusto.saad@gmail.com>
2026-07-28 13:23:08 -0300

    Cap 3 - Recursão

---
+++ Cap_3/pilha_de_chamada.py
@@ -0,0 +1,14 @@
+def sauda(nome):
+    print("Olá, " + nome + "!")
+    sauda2(nome)
+    print("preparando para dizer tchau...")
+    tchau()
+
+def sauda2(nome):
+    print("Como vai " + nome + "?")
+
+def tchau():
+    print("Ok, tchau!")
+
+
+sauda("Gabriel")
